6351933b325abd099c434616a60bd132daa63fc6
Sazzad <sazzad47.ju@gmail.com>
2022-11-06 13:04:49 -0800

    make it better

--- components/global/Blocks.tsx
+++ components/global/Blocks.tsx
@@ -9,20 +9,24 @@ const cards = [
 ]
 const Blocks = () => {
   return (
-    <div className='w-full mt-5'>
-        <Grid container spacing={2}>
+    <div className='w-full mt-2 p-5'>
+        <Grid container spacing={4}>
            {cards.map((item, i) => (
-             <Grid key={i} item xs={12} md={4} className="flex justify-center mt-5">
+             <Grid key={i} item xs={12} md={4} className="flex justify-center">
                <div className='global_service_block_container'>
                 <div className='global_service_block face1'>
-                    <PlayCircleFilledIcon/>
-                    <h3>Beta</h3>
-                  <div className='global_service_block_content'>
-                    <p></p>
-                  </div>
+                    <div className='global_service_block_content flex flex-col items-center justify-center'>
+                        <PlayCircleFilledIcon className='icon' />
+                        <h3>Beta</h3>
+                    </div>
+                  
                 </div>
                 <div className='global_service_block face2'>
-
+                  <div className='global_service_block_content flex flex-col items-center justify-center'>
+                        <p>
+                        Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, DTube is a great YouTube alternative.
+                        </p>
+                   </div>
                 </div>
                  
                </div>
