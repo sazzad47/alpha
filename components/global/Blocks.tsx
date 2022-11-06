8355299f61ee2f6286d154dec736508ecf4eca43
Sazzad <sazzad47.ju@gmail.com>
2022-11-06 13:43:06 -0800

    make it better

--- components/global/Blocks.tsx
+++ components/global/Blocks.tsx
@@ -1,31 +1,68 @@
 import React from 'react'
-import {Grid, Card, Typography} from '@mui/material'
-import logo from '../../public/logo.png'
-import Image from 'next/image'
+import {Grid} from '@mui/material'
 import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+import OfflineBoltIcon from '@mui/icons-material/OfflineBolt';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import Link from 'next/link';
 
 const cards = [
-    1, 2, 3, 4, 5, 6
+    {id: 1,
+    icon: <PlayCircleFilledIcon className='icon'/>,
+    name: 'Beta',
+    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, Beta is a great YouTube alternative.',
+    path: '/beta'
+    },
+    {id: 2,
+    icon: <OfflineBoltIcon className='icon'/>,
+    name: 'Gamma',
+    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, Beta is a great YouTube alternative.',
+    path: '/gamma'
+    },
+    {id: 3,
+    icon: <WhatsAppIcon className='icon'/>,
+    name: 'Delta',
+    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, Beta is a great YouTube alternative.',
+    path: '/delta'
+    },
+    {id: 4,
+    icon: <PlayCircleFilledIcon className='icon'/>,
+    name: 'Beta',
+    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, Beta is a great YouTube alternative.',
+    path: '/beta'
+    },
+    {id: 5,
+    icon: <PlayCircleFilledIcon className='icon'/>,
+    name: 'Beta',
+    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, Beta is a great YouTube alternative.',
+    path: '/beta'
+    },
+    {id: 6,
+    icon: <PlayCircleFilledIcon className='icon'/>,
+    name: 'Beta',
+    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, Beta is a great YouTube alternative.',
+    path: '/beta'
+    },
 ]
 const Blocks = () => {
   return (
     <div className='w-full mt-2 p-5'>
         <Grid container spacing={4}>
-           {cards.map((item, i) => (
-             <Grid key={i} item xs={12} md={4} className="flex justify-center">
+           {cards.map((item) => (
+             <Grid key={item.id} item xs={12} md={4} className="flex justify-center">
                <div className='global_service_block_container'>
                 <div className='global_service_block face1'>
                     <div className='global_service_block_content flex flex-col items-center justify-center'>
-                        <PlayCircleFilledIcon className='icon' />
-                        <h3>Beta</h3>
+                         {item.icon}
+                        <h3>{item.name}</h3>
                     </div>
                   
                 </div>
                 <div className='global_service_block face2'>
                   <div className='global_service_block_content flex flex-col items-center justify-center'>
                         <p>
-                        Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, DTube is a great YouTube alternative.
+                        {item.desc}
                         </p>
+                        <Link href={`${item.path}`}><button className='mt-3'>Explore</button></Link>
                    </div>
                 </div>
                  
