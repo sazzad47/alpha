6ea950497b792a27bc0349dad1eb5cd2ef2e5fa7
Sazzad <sazzad47.ju@gmail.com>
2022-11-07 09:32:57 -0800

    make it better

--- components/global/Blocks.tsx
+++ components/global/Blocks.tsx
@@ -12,64 +12,60 @@ const cards = [
     {id: 1,
     icon: <PlayCircleFilledIcon className='icon'/>,
     name: 'Beta',
-    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, Beta is a great YouTube alternative.',
+    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers.',
     path: '/beta'
     },
     {id: 2,
     icon: <OfflineBoltIcon className='icon'/>,
     name: 'Gamma',
-    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, Beta is a great YouTube alternative.',
+    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers.',
     path: '/gamma'
     },
     {id: 3,
     icon: <WhatsAppIcon className='icon'/>,
     name: 'Delta',
-    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, Beta is a great YouTube alternative.',
+    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers.',
     path: '/delta'
     },
     {id: 4,
     icon: <TravelExploreIcon className='icon'/>,
     name: 'Epsilon',
-    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, Beta is a great YouTube alternative.',
+    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers.',
     path: '/epsilon'
     },
     {id: 5,
     icon: <LocationSearchingIcon className='icon'/>,
     name: 'Zeta',
-    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, Beta is a great YouTube alternative.',
+    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers.',
     path: '/zeta'
     },
     {id: 6,
     icon: <RadarIcon className='icon'/>,
     name: 'Eta',
-    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers. Thus, Beta is a great YouTube alternative.',
+    desc: 'Beta is the best video sharing platform after YouTube. This makes the data safe from hackers.',
     path: '/eta'
     },
 ]
 const Blocks = () => {
   return (
     <div className='global_service_section w-full p-5'>
-        <Grid container spacing={4}>
+        <Grid container spacing={2} className="pt-[8rem]">
            {cards.map((item) => (
              <Grid key={item.id} item xs={12} md={4} className="flex justify-center">
+              <Link href={`${item.path}`}>
                <div className='global_service_block_container'>
-                <div className='global_service_block face1'>
-                    <div className='global_service_block_content flex flex-col items-center justify-center'>
+                <div className='global_service_block face1 bg-rose-700 hover:bg-rose-900'>
+                    <div className='global_service_block_content flex flex-col items-center justify-center p-4'>
                          {item.icon}
-                        <h3>{item.name}</h3>
+                        <h3 className='text-2xl text-slate-50'>{item.name}</h3>
+                        <p className="mt-3 text-sm text-center text-slate-100">
+                        {item.desc}
+                        </p>
                     </div>
                   
                 </div>
-                <div className='global_service_block face2'>
-                  <div className='global_service_block_content flex flex-col items-center justify-center'>
-                        <p>
-                        {item.desc}
-                        </p>
-                        <Link href={`${item.path}`}><button className='mt-3'>Explore</button></Link>
-                   </div>
-                </div>
-                 
                </div>
+              </Link>
              </Grid>
            ))}
         </Grid>
