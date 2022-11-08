8e04307f9e8f03cde546de34c3bdde2c1980aaae
Sazzad <sazzad47.ju@gmail.com>
2022-11-08 06:41:52 -0800

    make it better

--- components/global/Reviews.tsx
+++ components/global/Reviews.tsx
@@ -11,8 +11,8 @@ import { Navigation, Pagination, Scrollbar, A11y, Autoplay  } from "swiper";
 const Reviews = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center  py-[10rem] md:py-[5rem] bg-gray-800 w-full">
-        <div className="board">
+      <div className="flex flex-col items-center justify-center px-5 py-[10rem] md:py-[5rem] bg-gray-800 w-full">
+        <div className="board min-w-full">
           <h2 className="text-slate-50 text-2xl p-3 mt-1 md:mt-0">Reviews from our verified users.</h2>
           <Swiper
             modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay ]}
