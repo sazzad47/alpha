e42bfdee8c126b78e1cf12b31e3e0d77a3a5d1c3
Sazzad <sazzad47.ju@gmail.com>
2022-11-30 12:26:16 -0800

    make it better

--- components/beta/playlists/Layout.tsx
+++ components/beta/playlists/Layout.tsx
@@ -8,18 +8,44 @@ import CommentBox from "../videos/watch/comment";
 import Playlist from "./Playlist";
 
 const Index = () => {
+  const [showCommentBox, setShowCommentBox] = React.useState(false);
+  const [showReportBox, setShowReportBox] = React.useState(false);
+  const [showInfoBox, setShowInfoBox] = React.useState(false);
+
   const [theaterMode, setTheaterMode] = useState<boolean>(false);
   const toggleTheater = () => {
     setTheaterMode((prev) => !prev);
+  };
+  const handleShowCommentBox = () => {
+    setShowCommentBox(!showCommentBox);
+    setShowReportBox(false);
+    setShowInfoBox(false);
+  };
+  const handleShowReportBox = () => {
+    setShowReportBox(!showReportBox);
+    setShowCommentBox(false);
+    setShowInfoBox(false);
+  };
+  const handleShowInfoBox = () => {
+    setShowInfoBox(!showInfoBox);
+    setShowCommentBox(false);
+    setShowReportBox(false);
   };
   return (
     <Grid className="min-h-[100vh] px-5 w-full max-w-full py-[5rem] bg-[#eee] dark:bg-[#242526]">
       <Grid container spacing={2} className={`${theaterMode? "hidden" : "flex"}`}>
         <Grid item xs={12} md={7}>
           <Watch toggleTheater={toggleTheater} />
-          <Action />
-          <Description />
-          <CommentBox />
+          <Action
+            handleShowCommentBox={handleShowCommentBox}
+            handleShowReportBox={handleShowReportBox}
+            handleShowInfoBox={handleShowInfoBox}
+          />
+          {showInfoBox && <Description />}
+          {showCommentBox && <CommentBox />}
+          <Grid className="mt-5">
+            <Suggestions />
+          </Grid>
         </Grid>
         <Grid item xs={12} md={5} className="flex flex-col">
           <Playlist/>
@@ -31,9 +57,16 @@ const Index = () => {
           <Watch toggleTheater={toggleTheater} />
         </Grid>
         <Grid item xs={12} md={7}>
-          <Action />
-          <Description />
-          <CommentBox />
+        <Action
+            handleShowCommentBox={handleShowCommentBox}
+            handleShowReportBox={handleShowReportBox}
+            handleShowInfoBox={handleShowInfoBox}
+          />
+          {showInfoBox && <Description />}
+          {showCommentBox && <CommentBox />}
+          <Grid className="mt-5">
+            <Suggestions />
+          </Grid>
         </Grid>
         <Grid item xs={12} md={5}>
           <Suggestions />
