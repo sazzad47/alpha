e498160c22fddbeb6323a258d33f15360c46f422
Sazzad <sazzad47.ju@gmail.com>
2022-12-20 18:00:10 -0800

    make it better

--- components/delta/chat/ChatBox.tsx
+++ components/delta/chat/ChatBox.tsx
@@ -5,43 +5,34 @@ const ChatBox = () => {
   return (
     <Grid className="px-5 py-5 flex flex-col gap-3 justify-end">
       <Grid className="w-auto flex flex-col items-end justify-end">
-      <Typography className="px-3 py-3 bg-[#54e6ae]">Where are you now? I am searching for you for two days.</Typography>
+      <Typography className="px-3 py-3 bg-[#54e6ae] dark:bg-[#66814b33]">Where are you now? I am searching for you for two days.</Typography>
       </Grid>
       <Grid className="w-auto flex flex-col items-start justify-end">
-      <Typography className="px-3 py-3 bg-[#fff]">Where are you now? I am searching for you for two days.</Typography>
+      <Typography className="px-3 py-3 bg-[#fff] dark:bg-[#bcd4bc55]" >Where are you now? I am searching for you for two days.</Typography>
       </Grid>
       <Grid className="w-auto flex flex-col items-end justify-end">
-      <Typography className="px-3 py-3 bg-[#54e6ae]">Where are you now? I am searching for you for two days.</Typography>
+      <Typography className="px-3 py-3 bg-[#54e6ae] dark:bg-[#66814b33]">Where are you now? I am searching for you for two days.</Typography>
       </Grid>
       <Grid className="w-auto flex flex-col items-start justify-end">
-      <Typography className="px-3 py-3 bg-[#fff]">Where are you now? I am searching for you for two days.</Typography>
+      <Typography className="px-3 py-3 bg-[#fff] dark:bg-[#bcd4bc55]" >Where are you now? I am searching for you for two days.</Typography>
       </Grid>
       <Grid className="w-auto flex flex-col items-end justify-end">
-      <Typography className="px-3 py-3 bg-[#54e6ae]">Where are you now? I am searching for you for two days.</Typography>
+      <Typography className="px-3 py-3 bg-[#54e6ae] dark:bg-[#66814b33]">Where are you now? I am searching for you for two days.</Typography>
       </Grid>
       <Grid className="w-auto flex flex-col items-start justify-end">
-      <Typography className="px-3 py-3 bg-[#fff]">Where are you now? I am searching for you for two days.</Typography>
+      <Typography className="px-3 py-3 bg-[#fff] dark:bg-[#bcd4bc55]" >Where are you now? I am searching for you for two days.</Typography>
       </Grid>
       <Grid className="w-auto flex flex-col items-end justify-end">
-      <Typography className="px-3 py-3 bg-[#54e6ae]">Where are you now? I am searching for you for two days.</Typography>
+      <Typography className="px-3 py-3 bg-[#54e6ae] dark:bg-[#66814b33]">Where are you now? I am searching for you for two days.</Typography>
       </Grid>
       <Grid className="w-auto flex flex-col items-start justify-end">
-      <Typography className="px-3 py-3 bg-[#fff]">Where are you now? I am searching for you for two days.</Typography>
+      <Typography className="px-3 py-3 bg-[#fff] dark:bg-[#bcd4bc55]" >Where are you now? I am searching for you for two days.</Typography>
       </Grid>
       <Grid className="w-auto flex flex-col items-end justify-end">
-      <Typography className="px-3 py-3 bg-[#54e6ae]">Where are you now? I am searching for you for two days.</Typography>
+      <Typography className="px-3 py-3 bg-[#54e6ae] dark:bg-[#66814b33]">Where are you now? I am searching for you for two days.</Typography>
       </Grid>
       <Grid className="w-auto flex flex-col items-start justify-end">
-      <Typography className="px-3 py-3 bg-[#fff]">Where are you now? I am searching for you for two days.</Typography>
-      </Grid>
-      <Grid className="w-auto flex flex-col items-end justify-end">
-      <Typography className="px-3 py-3 bg-[#54e6ae]">Where are you now? I am searching for you for two days.</Typography>
-      </Grid>
-      <Grid className="w-auto flex flex-col items-start justify-end">
-      <Typography className="px-3 py-3 bg-[#fff]">Where are you now? I am searching for you for two days.</Typography>
-      </Grid>
-      <Grid className="w-auto flex flex-col items-end justify-end">
-      <Typography className="px-3 py-3 bg-[#54e6ae]">Where are you now? I am searching for you for two days.</Typography>
+      <Typography className="px-3 py-3 bg-[#fff] dark:bg-[#bcd4bc55]" >Where are you now? I am searching for you for two days.</Typography>
       </Grid>
     </Grid>
   )
